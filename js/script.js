fbec6e01c0a1c85f53cb34f5a7c5ed6ec4cac61c
RoGo117 <162340522+RoGo117@users.noreply.github.com>
2024-12-09 13:50:47 +0100

    Delete js directory
    Eliminar carpeta antigua de JS

--- js/script.js
+++
@@ -1,7 +0,0 @@
- // Menú móvil
- const menubtn = document.getElementById('menubtn');
- const mobilemenu = document.getElementById('mobilemenu');
-
- menubtn.addEventListener('click', () => {
-   mobilemenu.classList.toggle('hidden');
- });
