8db0b310bb93586c2c14d1ed086da8d225fc769c
RoGo117 <rodrigoclases117@gmail.com>
2024-12-09 02:07:59 +0100

    Página index y form completas + estructura de archivos modificada

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+/*
+    JS Script
+    -Rodrigo Moreno Bielsa
+
+*/
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
